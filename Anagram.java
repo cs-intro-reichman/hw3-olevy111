ebbc6217393b372b9856ce97260166ceb54c7984
Or Levy <olevy111@gmail.com>
2024-11-21 21:54:40 +0200

    update

--- Anagram.java
+++ Anagram.java
@@ -75,7 +75,7 @@ public class Anagram {
             String word = "";
 		for (int i=0; i<str.length(); i++)
 		{
-			if ((str.charAt(i)>= 'a' && str.charAt(i)<= 'z'))
+			if ((str.charAt(i)>= 'a' && str.charAt(i)<= 'z') || str.charAt(i) == ' ')
 			{
 				word = word + str.charAt(i);
 			}
